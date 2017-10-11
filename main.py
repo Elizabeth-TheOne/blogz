ba1c3df20373645feb791a55ff31127075920c4a
Elizabeth <Elizabeth325325@gmail.com>
2017-10-10 23:32:27 -0500

    changed some code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build_a_blog:123456@localhost:8889/build_a_blog'
@@ -8,14 +9,13 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
-    id = db.Colum(db.INteger, primary_key=True)
-    title = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    title = db.Column(db.String(100))
 
-@app.route('/newpost', methods=['GET', 'POST'])
-
-def add_blog():
-    return render_template('')
-
+def _init_(self, title, body):
+    self.title = title
+    self.body = body    
 
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
@@ -28,14 +28,24 @@ def index():
         blogs = Blog.query.all()
         
         return render_template('blog.html')
+        
+@app.route('/newpost', methods=['GET', 'POST'])
+def add_blog():
+    if request.method == 'GET':
+         return render_template('newpost.html', title=add_blog_entry)
 
-    
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
+    if request.method == 'POST':    
+        blog_title = request.form('title')
+        blog_body = request.form('body')
+        title_error = ""
+        body_error = ""
 
+    if len(blog_title) < 1:
+        title_error = "Invalid Title"
 
+    if len(blog_body) < 1:
+        body_error = "Invalid Body"
+   
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
 
@@ -46,12 +56,13 @@ def delete_task():
     db.session.commit()
 
     return redirect('/')
-if not title_error and not body_error:
-    new_blog = Blog(blog_title, blog_body
-    db.session.sdd(new_blog)
-    db.session.commit()
-    query_parm_url = "/blog?id+" +str(new_blog.id)
-    return redirect(query_parm_url)
 
-if __name__ == '__main__':
-    app.run()
+    if not title_error and not body_error:
+        new_blog = Blog(blog_title, blog_body)
+        db.session.sdd(new_blog)
+        db.session.commit()
+        query_parm_url = "/blog?id+" + str(new_blog.id)    
+        return redirect(query_parm_url)
+
+    if __name__ == '__main__':
+         app.run()
