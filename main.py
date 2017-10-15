bc0a371edd5059a2df618e0176af641e01f60948
Elizabeth <Elizabeth325325@gmail.com>
2017-10-15 12:21:21 -0500

    coded

--- main.py
+++ main.py
@@ -3,18 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build_a_blog:123456@localhost:8889/build_a_blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:123456@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(50))
     body = db.Column(db.String(100))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
-        self.body = body    
+        self.body = body  
+        self.owner = owner  
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
 
 @app.route('/blog')
 def display_blog():
